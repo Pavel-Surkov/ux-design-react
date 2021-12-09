ccbda7a20fa3f23b3cbe11143250c0b5f4d5ea38
Pavel Surkov <surkovv.pavel@gmail.com>
2021-12-09 22:49:42 +0700

    adds logic to course select

--- app/js/babel/components/PaymentCourseSelect.jsx
+++ app/js/babel/components/PaymentCourseSelect.jsx
@@ -6,7 +6,9 @@ import { currentRoot } from '../variables';
 
 export const PaymentCourseSelect = () => {
 	const coursesUrl = `${currentRoot}/data/paymentCourses.json`;
+
 	const formElementRef = useRef(null);
+	const selectElementRef = useRef(null);
 
 	useEffect(() => {
 		if (isEmpty(paymentStore.data)) {
@@ -14,15 +16,47 @@ export const PaymentCourseSelect = () => {
 		}
 	}, []);
 
-	const handleClick = e => {
-		// TODO: Add handler here for select click
-		console.log(paymentStore.data);
+	const handleBtnClick = e => {
+		const target = e.target;
+
+		const formEl = formElementRef.current;
+		const courseList = target.nextElementSibling;
+
+		target.classList.toggle('ums-select__btn_state-active');
+		formEl.classList.toggle('form__select_state-active');
+		courseList.classList.toggle('ums-select__list_visibility-open');
+	};
+
+	const handleCourseClick = e => {
+		const target = e.target;
+
+		const formEl = formElementRef.current;
+		const courseSelect = selectElementRef.current;
+		const courseList = courseSelect.nextElementSibling;
+
+		target.classList.add('ums-select__list-item_state-active');
+		formEl.classList.toggle('form__select_state-active');
+		courseList.classList.toggle('ums-select__list_visibility-open');
+		courseSelect.classList.toggle('ums-select__btn_state-active');
+
+		if (paymentStore.currentValues.previousElementItem) {
+			const oldCourse = paymentStore.currentValues.previousElementItem;
+
+			oldCourse.classList.remove('ums-select__list-item_state-active');
+			paymentStore.setPreviousElementItem(target);
+			paymentStore.setSelectedCourse(target.innerHTML);
+		} else {
+			paymentStore.setPreviousElementItem(target);
+			paymentStore.setSelectedCourse(target.innerHTML);
+		}
+
+		courseSelect.innerHTML = target.innerHTML;
 	};
 
 	return (
 		<div ref={formElementRef} className="form__input form__select payment-form__select payment-form__input">
 			<div className="ums-select">
-				<button className="ums-select__btn" onClick={handleClick}>
+				<button ref={selectElementRef} className="ums-select__btn" onClick={handleBtnClick}>
 					Нажмите, чтобы выбрать курс
 				</button>
 				<ul className="ums-select__list">
@@ -36,7 +70,11 @@ export const PaymentCourseSelect = () => {
 									const [name, date, place] = course.name.split(', ');
 
 									return (
-										<li className="ums-select__list-item" key={course.name}>
+										<li
+											className="ums-select__list-item"
+											onClick={handleCourseClick}
+											key={course.name}
+										>
 											{`${date} – ${name} `}
 											<span>{`(${place})`}</span>
 										</li>
--- app/js/babel/stores/paymentStore.js
+++ app/js/babel/stores/paymentStore.js
@@ -6,11 +6,25 @@ const createPaymentStore = () => {
 			data: {},
 			setData(data) {
 				this.data = JSON.parse(JSON.stringify(data));
+			},
+			// For managing state of course select
+			currentValues: {
+				selectedCourse: null,
+				previousElementItem: null
+			},
+			setSelectedCourse(course) {
+				this.currentValues.selectedCourse = course;
+			},
+			setPreviousElementItem(el) {
+				this.currentValues.previousElementItem = el;
 			}
 		},
 		{
 			data: observable,
-			setData: action
+			setData: action,
+			currentValues: observable,
+			setSelectedCourse: action,
+			setPreviousElementItem: action
 		}
 	);
 };
