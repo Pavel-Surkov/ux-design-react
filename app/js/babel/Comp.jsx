2a417f914215b4a9c28d9c1196e4dea8c7a087b3
Pavel Surkov <surkovv.pavel@gmail.com>
2021-12-09 15:18:06 +0700

    Clears React components

--- app/js/babel/Comp.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export const Comp = () => {
-	return <a href="#">COMP</a>;
-};
--- app/js/babel/index.js
+++ app/js/babel/index.js
@@ -1,21 +1,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import { Comp } from './Comp.jsx';
 
-const Test = () => {
-	const [value, setValue] = useState('red');
+const App = () => {};
 
-	const handleClick = () => {
-		alert('Clicked!');
-	};
-
-	return (
-		<div>
-			<p>Le Go!</p>
-			<button onClick={handleClick}>GO!</button>
-			<Comp />
-		</div>
-	);
-};
-
-ReactDOM.render(<Test />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
